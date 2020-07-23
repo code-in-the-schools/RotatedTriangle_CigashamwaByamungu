c4ad6a458f114e5e8fb221f7ffbb8f09916ada50
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-23 17:22:36 +0000

    Everything

---
+++ main.py
@@ -0,0 +1,11 @@
+#UserInputFunctions
+L1 = input('Enter any character:')
+L2 = int(input(' Enter any positive number(>7):'))
+ 
+#ForLoops/Multipliers
+for i in range(2, L2 + 2):
+  anything = ""
+  for j in range(i):
+    anything+=L1
+
+    print(anything)
